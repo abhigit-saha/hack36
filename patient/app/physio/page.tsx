aa91f8e105ce49aff1581d56010a15fb8b7d0a82
alwaysahustler <sanjayghanhsyam5@gmail.com>
2025-04-19 16:33:40 +0530

    adds

--- patient/app/physio/page.tsx
+++ patient/app/physio/page.tsx
@@ -1,15 +1,15 @@
-import LiveDemo from "@/components/liveVideo";
-import VideoDemo from "@/components/recordedVideo";
+import LiveDemo from "../../components/liveVideo"; // Relative path to component
+import VideoDemo from "../../components/recordedVideo"; // Relative path to component
 
-import React from 'react'
+import React from 'react';
 
 const page = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <LiveDemo/>
-      <VideoDemo/>
+      <LiveDemo />
+      <VideoDemo />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
